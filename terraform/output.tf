c2904edba9acb3c64c1a958a9bb80dc4762797bf
Yu Zhang <zhangy@YudeMacBook-Pro.local>
2024-11-17 21:58:34 +1100

    explaination of FQDN

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,7 +4,13 @@ output "vm_hostnames" {
 }
 
 
-output "vm_ip_addresses" {
+output "vm_external_ip_addresses" {
   value       = { for instance, vm in google_compute_instance.vm : instance => vm.network_interface[0].access_config[0].nat_ip }
   description = "A map of instance FQDN to their respective external IP addresses"
+}
+
+
+output "vm_internal_ip_addresses" {
+  value       = { for instance, vm in google_compute_instance.vm : instance => vm.network_interface[0].network_ip }
+  description = "A map of instance FQDN to their respective internal IP addresses"
 }
